542935ad78cf882643b697033fbc042da1f87373
Michael Cardiff <mcardiff@hawk.iit.edu>
2022-01-21 16:34:24 -0600

    Initial Commit

---
+++ Shared/HW_1App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_1App.swift
+//  Shared
+//
+//  Created by Michael Cardiff on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct HW_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
