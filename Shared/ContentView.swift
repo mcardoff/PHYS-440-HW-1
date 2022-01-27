e811bba30467f1fede7e0276953c23b57f7226a5
Michael Cardiff <mcardiff@hawk.iit.edu>
2022-01-26 20:29:32 -0600

    Change ContentView to give a better looking UI

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,94 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject private var sphereModel = Sphere()
+    @State var radiusString = "1.0"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+            Text("Radius")
+                .padding(.top)
+                .padding(.bottom, 0)
+            TextField("Enter Radius", text: $radiusString, onCommit: {Task.init {await self.calculateCircle()}})
+                .padding(.horizontal)
+                .frame(width: 100)
+                .padding(.top, 0)
+                .padding(.bottom, 30)
+            HStack {
+                VStack{
+            Text("Volume")
+                .padding(.bottom, 0)
+            TextField("", text: $sphereModel.volumeText)
+                .padding(.horizontal)
+                .frame(width: 100)
+                .padding(.top, 0)
+                .padding(.bottom,30)
+            
+        }
+        VStack{
+            Text("Volume")
+                .padding(.bottom, 0)
+            Text("\(sphereModel.volume, specifier: "%.2f")")
+                .padding(.horizontal)
+                .frame(width: 100)
+                .padding(.top, 0)
+                .padding(.bottom,30)
+                
+            }
+            }
+            HStack{
+                VStack{
+            Text("Surface Area")
+                .padding(.bottom, 0)
+            TextField("", text: ( $sphereModel.surfaceAreaText))
+                .padding(.horizontal)
+                .frame(width: 100)
+                .padding(.top, 0)
+                .padding(.bottom, 30)
+                }
+                VStack{
+                    Text("Surface Area")
+                        .padding(.bottom, 0)
+                    Text("\(sphereModel.surfaceArea, specifier: "%.2f")")
+                        .padding(.horizontal)
+                        .frame(width: 100)
+                        .padding(.top, 0)
+                        .padding(.bottom,30)
+                        
+                    }
+
+                
+                
+            }
+            
+            Button("Calculate", action: {Task.init { await self.calculateCircle()}})
+                .padding(.bottom)
+                .padding()
+                .disabled(sphereModel.enableButton == false)
+            
+            
+        }
+        
     }
+    
+    func calculateCircle() async {
+        
+        sphereModel.setButtonEnable(state: false)
+        
+        let _ : Bool = await sphereModel.initWithRadius(radius: Double(radiusString)!)
+        
+        
+    
+
+}
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
     }
 }
