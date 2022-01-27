bfbf925fe5ad581601fe917dd0d457a9f681589e
Michael Cardiff <mcardiff@hawk.iit.edu>
2022-01-26 21:14:15 -0600

    Add comments to functions in Circle class

--- Shared/Sphere.swift
+++ Shared/Sphere.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 class Sphere: NSObject, ObservableObject {
     
-    
+    // necessary variables for calculating and displaying everything on screen
     var radius = 1.0
     var center = (x:0.0, y:0.0, z:0.0)
     @Published var volume = 0.0
@@ -19,26 +19,24 @@ class Sphere: NSObject, ObservableObject {
     @Published var enableButton = true
     
     
+    /// initWithRadius
+    /// initializes the sphere object with a given radius, as well as calculates its area
+    ///  - Parameter radius: Double precision value of the radius, defaults to 1
     func initWithRadius(radius: Double) async -> Bool {
         
         self.radius = radius
-       
             let _ = await withTaskGroup(of:  Void.self) { taskGroup in
-                taskGroup.addTask { let _ = await self.calculateArea(radius:radius)}
+                taskGroup.addTask { let _ = await self.calculateVolume(radius:radius)}
                 taskGroup.addTask { let _ = await self.calculateSurfaceArea(radius: radius)}
-            
         }
-            
             await setButtonEnable(state: true)
-                                                 
-       
-        
-
         return true
-        
     }
     
-    func calculateArea(radius: Double) async -> Double {
+    /// calculateVolume
+    /// Calculates the sphere's volume using the analytic formula
+    ///  - Parameter radius: Double precision value of the radius, defaults to 1
+    func calculateVolume(radius: Double) async -> Double {
         
         // V = (4/3) * pi * r^3
         
@@ -49,10 +47,11 @@ class Sphere: NSObject, ObservableObject {
         await newVolumeValue(volume: calculatedVolume)
         
         return calculatedVolume
-        
-        
     }
     
+    /// calculateSurfaceArea
+    /// Calculates the sphere's surface area using the analytic formula
+    ///  - Parameter radius: Double precision value of the radius, defaults to 1
     func calculateSurfaceArea(radius: Double) async -> Double {
         
         // SA = 4 * Pi * r^2
@@ -67,6 +66,9 @@ class Sphere: NSObject, ObservableObject {
         
     }
     
+    /// setButtonEnable
+    /// Toggle the state of the Button on the Main Thread
+    /// - Parameter state: Boolean describing whether the button should be pushable.
     @MainActor func setButtonEnable(state: Bool){
         if state {
             Task.init {
@@ -74,8 +76,7 @@ class Sphere: NSObject, ObservableObject {
                     self.enableButton = true
                 }
             }
-        }
-        else{
+        } else {
             Task.init {
                 await MainActor.run {
                     self.enableButton = false
@@ -84,10 +85,6 @@ class Sphere: NSObject, ObservableObject {
         }
     }
     
-
-    @MainActor func updateVolume(volumeTextString: String){
-        volumeText = volumeTextString
-    }
     
     @MainActor func newVolumeValue(volume: Double){
         self.volume = volume
@@ -97,6 +94,16 @@ class Sphere: NSObject, ObservableObject {
         self.surfaceArea = surfaceArea
     }
     
+    /// updateVolume
+    /// Update the volumeText variable
+    /// - Parameter volumeTextString: String converted from a Double which contains the volume
+    @MainActor func updateVolume(volumeTextString: String){
+        volumeText = volumeTextString
+    }
+    
+    /// updateSurfaceArea
+    /// Update the surfaceAreaText variable
+    /// - Parameter volumeTextString: String converted from a Double which contains the calculated surface area
     @MainActor func updateSurfaceArea(surfaceAreaTextString:String){
         surfaceAreaText = surfaceAreaTextString
     }
