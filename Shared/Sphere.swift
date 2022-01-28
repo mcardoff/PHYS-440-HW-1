db501b91b25f81dc01eb310b271d7ff61fec9af4
Michael Cardiff <mcardiff@hawk.iit.edu>
2022-01-27 21:09:27 -0600

    Adapt Sphere class to work with Shape

--- Shared/Sphere.swift
+++ Shared/Sphere.swift
@@ -7,19 +7,11 @@
 
 import SwiftUI
 
-class Sphere: NSObject, ObservableObject {
+class Sphere: Shape  {
     
     // necessary variables for calculating and displaying everything on screen
     var radius = 1.0
-    var center = (x:0.0, y:0.0, z:0.0)
-    // updated when initWithRadius is called
-    @Published var volume = 0.0
-    @Published var surfaceArea = 0.0
-    @Published var volumeText = ""
-    @Published var surfaceAreaText = ""
-    // changed in contentView
-    @Published var enableButton = true
-    
+    var center = (x:0.0, y:0.0, z:0.0)    
     
     /// initWithRadius
     /// initializes the sphere object with a given radius, as well as calculates its area
@@ -27,9 +19,9 @@ class Sphere: NSObject, ObservableObject {
     func initWithRadius(radius: Double) async -> Bool {
         
         self.radius = radius
-            let _ = await withTaskGroup(of:  Void.self) { taskGroup in
-                taskGroup.addTask { let _ = await self.calculateVolume(radius:radius)}
-                taskGroup.addTask { let _ = await self.calculateSurfaceArea(radius: radius)}
+            let _ = await withTaskGroup(of: Void.self) { taskGroup in
+                taskGroup.addTask { let _ = await self.calculateVolume(param: radius)}
+                taskGroup.addTask { let _ = await self.calculateSurfaceArea(param: radius)}
         }
             await setButtonEnable(state: true)
         return true
@@ -38,9 +30,10 @@ class Sphere: NSObject, ObservableObject {
     /// calculateVolume
     /// Calculates the sphere's volume using the analytic formula
     ///  - Parameter radius: Double precision value of the radius, defaults to 1
-    func calculateVolume(radius: Double) async -> Double {
+    override func calculateVolume(param: Double) async -> Double {
         
         // V = (4/3) * pi * r^3
+        let radius = param
         
         let calculatedVolume = (4.0/3.0) * Double.pi * pow(radius,3)
         let newVolumeText = String(format: "%7.5f", calculatedVolume)
@@ -54,9 +47,10 @@ class Sphere: NSObject, ObservableObject {
     /// calculateSurfaceArea
     /// Calculates the sphere's surface area using the analytic formula
     ///  - Parameter radius: Double precision value of the radius, defaults to 1
-    func calculateSurfaceArea(radius: Double) async -> Double {
+    override func calculateSurfaceArea(param: Double) async -> Double {
         
         // SA = 4 * Pi * r^2
+        let radius = param
         
         let calculatedSurfaceArea = 4 * Double.pi * pow(radius,2)
         let newSurfaceAreaText = String(format: "%7.5f", calculatedSurfaceArea)
@@ -66,52 +60,5 @@ class Sphere: NSObject, ObservableObject {
         
         return calculatedSurfaceArea
         
-    }
-    
-    /// setButtonEnable
-    /// Toggle the state of the Button on the Main Thread
-    /// - Parameter state: Boolean describing whether the button should be pushable.
-    @MainActor func setButtonEnable(state: Bool){
-        if state {
-            Task.init {
-                await MainActor.run {
-                    self.enableButton = true
-                }
-            }
-        } else {
-            Task.init {
-                await MainActor.run {
-                    self.enableButton = false
-                }
-            }
-        }
-    }
-    
-    /// newVolumeValue
-    /// Setter function for the volume field of the sphere class
-    /// - Parameter volume: New volume value to update class
-    @MainActor func newVolumeValue(volume: Double){
-        self.volume = volume
-    }
-    
-    /// newSurfaceAreaValue
-    /// Setter function for the surface area field of the sphere class
-    /// - Parameter surfaceArea: New surface area value to update class
-    @MainActor func newSurfaceAreaValue(surfaceArea: Double){
-        self.surfaceArea = surfaceArea
-    }
-    
-    /// updateVolume
-    /// Update the volumeText variable
-    /// - Parameter volumeTextString: String converted from a Double which contains the volume
-    @MainActor func updateVolume(volumeTextString: String){
-        volumeText = volumeTextString
-    }
-    
-    /// updateSurfaceArea
-    /// Update the surfaceAreaText variable
-    /// - Parameter surfaceAreaTextString: String converted from a Double which contains the calculated surface area
-    @MainActor func updateSurfaceArea(surfaceAreaTextString:String){
-        surfaceAreaText = surfaceAreaTextString
     }
 }
