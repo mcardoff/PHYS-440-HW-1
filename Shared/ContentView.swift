9029f69ea926a4c0a37cda2f3b47ff6b21e7b74b
Michael Cardiff <mcardiff@hawk.iit.edu>
2022-01-27 21:40:04 -0600

    Add hstack that contains cube calculations

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -12,75 +12,147 @@ struct ContentView: View {
     @ObservedObject private var sphereModel = Sphere()
     @ObservedObject private var cubeModel = Cube()
     @State var radiusString = "1.0"
+    @State var sideLenString = "1.0"
     
     var body: some View {
         
-        VStack{
-            Text("Radius")
-                .padding(.top)
-                .padding(.bottom, 0)
-            TextField("Enter Radius",
-                      text: $radiusString,
-                      onCommit: {Task.init {await self.calculateSphere()}})
-                .padding(.horizontal)
-                .frame(width: 100)
-                .padding(.top, 0)
-                .padding(.bottom, 30)
-            HStack {
-                VStack{
-                    Text("Volume")
-                        .padding(.bottom, 0)
-                    TextField("",
-                              text: $sphereModel.volumeText)
-                        .padding(.horizontal)
-                        .frame(width: 100)
-                        .padding(.top, 0)
-                        .padding(.bottom,30)
-                }
-                
-                VStack{
-                    Text("Volume")
-                        .padding(.bottom, 0)
-                    Text("\(sphereModel.volume, specifier: "%.2f")")
-                        .padding(.horizontal)
-                        .frame(width: 100)
-                        .padding(.top, 0)
-                        .padding(.bottom,30)
+        HStack {
+            // Sphere
+            VStack{
+                Text("Radius")
+                    .padding(.top)
+                    .padding(.bottom, 0)
+                TextField("Enter Radius",
+                          text: $radiusString,
+                          onCommit: {Task.init {await self.calculateSphere()}})
+                    .padding(.horizontal)
+                    .frame(width: 100)
+                    .padding(.top, 0)
+                    .padding(.bottom, 30)
+                HStack {
+                    VStack{
+                        Text("Volume")
+                            .padding(.bottom, 0)
+                        TextField("",
+                                  text: $sphereModel.volumeText)
+                            .padding(.horizontal)
+                            .frame(width: 100)
+                            .padding(.top, 0)
+                            .padding(.bottom,30)
+                    }
                     
+                    VStack{
+                        Text("Volume")
+                            .padding(.bottom, 0)
+                        Text("\(sphereModel.volume, specifier: "%.2f")")
+                            .padding(.horizontal)
+                            .frame(width: 100)
+                            .padding(.top, 0)
+                            .padding(.bottom,30)
+                        
+                    }
                 }
-            }
-            HStack{
-                VStack{
-                    Text("Surface Area")
-                        .padding(.bottom, 0)
-                    TextField("", text: ( $sphereModel.surfaceAreaText))
-                        .padding(.horizontal)
-                        .frame(width: 100)
-                        .padding(.top, 0)
-                        .padding(.bottom, 30)
+                HStack{
+                    VStack{
+                        Text("Surface Area")
+                            .padding(.bottom, 0)
+                        TextField("", text: ( $sphereModel.surfaceAreaText))
+                            .padding(.horizontal)
+                            .frame(width: 100)
+                            .padding(.top, 0)
+                            .padding(.bottom, 30)
+                    }
+                    
+                    VStack{
+                        Text("Surface Area")
+                            .padding(.bottom, 0)
+                        Text("\(sphereModel.surfaceArea, specifier: "%.2f")")
+                            .padding(.horizontal)
+                            .frame(width: 100)
+                            .padding(.top, 0)
+                            .padding(.bottom,30)
+                    }
                 }
                 
-                VStack{
-                    Text("Surface Area")
-                        .padding(.bottom, 0)
-                    Text("\(sphereModel.surfaceArea, specifier: "%.2f")")
-                        .padding(.horizontal)
-                        .frame(width: 100)
-                        .padding(.top, 0)
-                        .padding(.bottom,30)
-                }
+                Button("Calculate", action: {Task.init { await self.calculateSphere()}})
+                    .padding(.bottom)
+                    .padding()
+                    .disabled(sphereModel.enableButton == false)
             }
             
-            Button("Calculate", action: {Task.init { await self.calculateSphere()}})
-                .padding(.bottom)
-                .padding()
-                .disabled(sphereModel.enableButton == false)
+            // Cube
+            VStack{
+                Text("Side Length")
+                    .padding(.top)
+                    .padding(.bottom, 0)
+                TextField("Enter Side Length",
+                          text: $sideLenString,
+                          onCommit: {Task.init {await self.calculateCube()}})
+                    .padding(.horizontal)
+                    .frame(width: 100)
+                    .padding(.top, 0)
+                    .padding(.bottom, 30)
+                HStack {
+                    VStack{
+                        Text("Volume")
+                            .padding(.bottom, 0)
+                        TextField("",
+                                  text: $cubeModel.volumeText)
+                            .padding(.horizontal)
+                            .frame(width: 100)
+                            .padding(.top, 0)
+                            .padding(.bottom,30)
+                    }
+                    
+                    VStack{
+                        Text("Volume")
+                            .padding(.bottom, 0)
+                        Text("\(cubeModel.volume, specifier: "%.2f")")
+                            .padding(.horizontal)
+                            .frame(width: 100)
+                            .padding(.top, 0)
+                            .padding(.bottom,30)
+                        
+                    }
+                }
+                HStack{
+                    VStack{
+                        Text("Surface Area")
+                            .padding(.bottom, 0)
+                        TextField("", text: ( $cubeModel.surfaceAreaText))
+                            .padding(.horizontal)
+                            .frame(width: 100)
+                            .padding(.top, 0)
+                            .padding(.bottom, 30)
+                    }
+                    
+                    VStack{
+                        Text("Surface Area")
+                            .padding(.bottom, 0)
+                        Text("\(cubeModel.surfaceArea, specifier: "%.2f")")
+                            .padding(.horizontal)
+                            .frame(width: 100)
+                            .padding(.top, 0)
+                            .padding(.bottom,30)
+                    }
+                }
+                
+                Button("Calculate", action: {Task.init { await self.calculateCube()}})
+                    .padding(.bottom)
+                    .padding()
+                    .disabled(cubeModel.enableButton == false)
+            }
         }
     }
     
     func calculateSphere() async {
         sphereModel.setButtonEnable(state: false)
         let _ : Bool = await sphereModel.initWithRadius(radius: Double(radiusString)!)
+    }
+    
+    func calculateCube() async {
+        cubeModel.setButtonEnable(state: false)
+        let _ : Bool = await cubeModel.initWithSideLength(sideLength: Double(radiusString)!)
     }
 }
 
