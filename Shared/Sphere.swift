1f607e30f74bb2284a0b1ac512a53f314379c70b
Michael Cardiff <mcardiff@hawk.iit.edu>
2022-01-28 20:05:28 -0600

    add skeleton for bounding box

--- Shared/Sphere.swift
+++ Shared/Sphere.swift
@@ -11,7 +11,11 @@ class Sphere: Shape {
     
     // necessary variables for calculating and displaying everything on screen
     var radius = 1.0
-    var center = (x:0.0, y:0.0, z:0.0)    
+    var center = (x:0.0, y:0.0, z:0.0)
+    @Published var boundingBoxVolume = 0.0
+    @Published var boundingBoxSA = 0.0
+    @Published var boundingBoxVolText = ""
+    @Published var boundingBoxSAText = ""
     
     /// initWithRadius
     /// initializes the sphere object with a given radius, as well as calculates its area
@@ -22,6 +26,8 @@ class Sphere: Shape {
             let _ = await withTaskGroup(of: Void.self) { taskGroup in
                 taskGroup.addTask { let _ = await self.calculateVolume(param: radius)}
                 taskGroup.addTask { let _ = await self.calculateSurfaceArea(param: radius)}
+                taskGroup.addTask { let _ = await self.calculateBoundingBoxVol(radius: radius)}
+                taskGroup.addTask { let _ = await self.calculateBoundingBoxSA(radius: radius)}
         }
             await setButtonEnable(state: true)
         return true
@@ -60,5 +66,49 @@ class Sphere: Shape {
         
         return calculatedSurfaceArea
         
+    }
+    
+    func calculateBoundingBoxVol(radius: Double) async -> Double {
+        
+        // V_BB = (2 * r) ^ 3
+        
+        let calculatedVol = pow(2.0 * radius, 3)
+        let newVolText = String(format: "%7.5f", calculatedVol)
+        
+        await updateBoundingBoxVol(boundBoxTextString: newVolText)
+        await newBoundingBoxVolValue(volume: calculatedVol)
+        
+        return calculatedVol
+        
+    }
+    
+    func calculateBoundingBoxSA(radius: Double) async -> Double {
+        
+        // SA_BB = 6 * (2 * r) ^ 2
+        
+        let calculatedSurfaceArea = 6 * pow(2 * radius, 2)
+        let newSurfaceAreaText = String(format: "%7.5f", calculatedSurfaceArea)
+        
+        await updateBoundingBoxSA(boundBoxTextString: newSurfaceAreaText)
+        await newBoundingBoxSAValue(surfaceArea: calculatedSurfaceArea)
+        
+        return calculatedSurfaceArea
+        
+    }
+    
+    @MainActor func newBoundingBoxVolValue(volume: Double) {
+        self.boundingBoxVolume = volume
+    }
+    
+    @MainActor func updateBoundingBoxVol(boundBoxTextString: String){
+        boundingBoxVolText = boundBoxTextString
+    }
+    
+    @MainActor func newBoundingBoxSAValue(surfaceArea: Double) {
+        self.boundingBoxSA = surfaceArea
+    }
+    
+    @MainActor func updateBoundingBoxSA(boundBoxTextString: String){
+        boundingBoxSAText = boundBoxTextString
     }
 }
